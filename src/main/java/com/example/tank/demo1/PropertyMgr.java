ef8af1cc089ea8d201d1079992d9384796f51ec4
刘阳桢 <liu.yangzhen@intellif.com>
2020-05-26 17:58:59 +0800

    三种非常好的单例模式实现，静态内部列，枚举单例，两次校验

--- src/main/java/com/example/tank/demo1/PropertyMgr.java
+++ src/main/java/com/example/tank/demo1/PropertyMgr.java
@@ -7,26 +7,58 @@ import java.util.Properties;
 
 /**
  * 功能说明：
+ * 单例模式
  *
  * @author LYZ
  * @date 2020/5/26 17:22
  */
+
 public class PropertyMgr {
+
+
+
+
+
+//    第三种 静态内部类保证单例，线程安全基于JVM，JVM每个类只会加载一次
     private PropertyMgr(){}
-    static Properties properties ;
-    private static PropertyMgr propertyMgr;
-    static {
-        try {
-            properties.load(new ClassPathResource("config").getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static class PropertyMgrHolder{
+        private static final  PropertyMgr propertyMgr = new PropertyMgr();
+    }
+    public static PropertyMgr getInstance(){
+        return PropertyMgrHolder.propertyMgr;
     }
 
-    public static PropertyMgr getInstance(){
-        if(propertyMgr==null){
-            propertyMgr = new PropertyMgr();
-        }
-        return propertyMgr;
-    }
+//    第二种单例，利用枚举完成单例  最完美的单例
+//    public enum PropertyMgrEnum{
+//        INTERFACE ;
+//
+//        public static void main(String[] args) {
+//            PropertyMgrEnum.INTERFACE.hashCode();
+//        }
+//    }
+
+
+
+//    第一种单利
+//    private PropertyMgr(){}
+//    static Properties properties ;
+//    private volatile static PropertyMgr propertyMgr;
+//    static {
+//        try {
+//            properties.load(new ClassPathResource("config").getInputStream());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static PropertyMgr getInstance(){
+//        if(propertyMgr==null){
+//            synchronized (PropertyMgr.class){
+//                if(propertyMgr==null){
+//                    propertyMgr = new PropertyMgr();
+//                }
+//            }
+//        }
+//        return propertyMgr;
+//    }
 }
